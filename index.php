404f6087c20c3ca61fe938b32aa43084bc131366
Hakim Mazouz <hakim@nomadic.dk>
2016-10-02 17:11:00 +0200

    add dashboard.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+	
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashy</title>
+</head>
+<body>
+	<h1>Hi! Welcome to Dashy. You can start by <a href="login.php">signing in</a>.</h1>
+	<p>Don't have an account? <a href="register.php">Register here</a>.</p>
+</body>
+</html>
